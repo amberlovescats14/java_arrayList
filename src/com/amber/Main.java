dd61de18f6dd70660b085e9bf925addf8ca3bc30
Amber Jones <amberlovescats14@gmail.com>
2019-10-11 08:19:12 -0500

    I created CRUD functionality for ArrayList

--- src/com/amber/Main.java
+++ src/com/amber/Main.java
@@ -7,5 +7,7 @@ public class Main {
         myPhoneBook.addNumber("210-429-6474");
         myPhoneBook.addNumber("210-705-9770");
         myPhoneBook.modify();
+        myPhoneBook.remove();
+        System.out.println(myPhoneBook.getPhoneBook());
     }
 }
--- src/com/amber/Phone.java
+++ src/com/amber/Phone.java
@@ -1,27 +1,30 @@
 package com.amber;
 
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Phone {
     private ArrayList<String> phoneBook = new ArrayList<String>();
 
+    //@ADD
     public void addNumber(String newNumber){
         phoneBook.add(newNumber);
         System.out.println("New number added.");
     }
+    //@does this contain
     private boolean containing(String number){
        return phoneBook.contains(number);
 
     }
-
+    //@ MODIFY
     public void modify(){
         Scanner scan = new Scanner(System.in);
         while(true){
             System.out.print("Please enter the number to modify: ");
             String currentNumber = scan.nextLine();
             if(containing(currentNumber)){
-                System.out.println("Enter updated number: ");
+                System.out.print("Enter updated number: ");
                 String newNumber = scan.nextLine();
                 int index = phoneBook.indexOf(currentNumber);
                 phoneBook.set(index, newNumber);
@@ -32,7 +35,22 @@ public class Phone {
         }
     }
 
+    public void remove(){
+        Scanner scan = new Scanner(System.in);
+        while (true){
+            System.out.print("Please enter the number to delete: ");
+        String currentNumber = scan.nextLine();
+            if(containing(currentNumber)){
+                int index = phoneBook.indexOf(currentNumber);
+                phoneBook.remove(index);
+                System.out.println("Number Deleted");
+                break;
+            }
+            System.out.println("404 Number Not found");
+        }
+    }
 
-
-
+    public ArrayList<String> getPhoneBook() {
+        return phoneBook;
+    }
 }
